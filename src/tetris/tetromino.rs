6f777ca3e8df6a26b37e570f09afba14678fa83a
Asta(아스타) <86705803+P-Asta@users.noreply.github.com>
2024-12-08 23:40:15 +0900

    CHORE: 주석 추가

--- src/tetris/tetromino.rs
+++ src/tetris/tetromino.rs
@@ -7,26 +7,32 @@ pub struct I {
     pub position: (i32, i32),
     pub rotate_index: i8,
 }
-
+/// I미노에 대한 블럭 설정입니다.
 impl Block for I {
     const ID: u8 = 0;
+    /// I미노를 초기화합니다.
     fn new(x: i32, y: i32) -> Self {
         I {
             position: (x, y),
             rotate_index: 0,
         }
     }
-
+    /// 시계방향 회전CW)을 하는 함수입니다.
     fn rotate_clockwise(&mut self) {
+        /// CW시에 srs)+ 월킥 테스트를 하는 변수입니다 
         let kick_tests = [(0, 0), (-2, 0), (2, 0), (-1, 1), (2, -1)];
 
         self.rotate_index.add(1);
     }
+    /// 반시계방향 회전(CCW)을 하는 함수입니다.
     fn rotate_counter_clockwise(&mut self) {
+        // CCW시에 srs+ 월킥을 테스트를 하는 변수입니다
         let kick_tests = [(0, 0), (-2, 0), (2, 0), (-1, 1), (2, -1)];
         self.rotate_index.sub(1);
     }
+    /// 180도 회전을 하는 함수입니다.
     fn rotate_180(&mut self) {
+        // 180회전시에 srs+ 월킥을 테스트를 하는 변수입니다
         let kick_tests = [
             (0, 0),
             (0, -1),
@@ -43,18 +49,22 @@ impl Block for I {
         self.rotate_index.add(2);
     }
 
+    /// 왼쪽으로 블럭을 옴김니다.
     fn move_left(&mut self) {
         self.position.0 -= 1;
     }
-
+    
+    /// 오른쪽으로 블럭을 옴김니다.
     fn move_right(&mut self) {
         self.position.0 += 1;
     }
-
+    
+    /// 소프트 드랍을 합니다.
     fn move_down(&mut self) {
         self.position.1 += 1;
     }
 
+    /// 소프트 드랍을 합니다.
     fn test_collision(&self, dx: i32, dy: i32, field: &GameField) -> bool {
         let shapes = self.get_shapes();
         let (x, y) = self.position;
