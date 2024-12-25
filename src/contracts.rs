a98e61ca2fb5e93c8f12321e10756151d8abe706
5-23 <yhanbyeol6@gmail.com>
2024-12-26 08:52:00 +0900

    feat: 아마도 i미노 완성

--- src/contracts.rs
+++ src/contracts.rs
@@ -1,16 +1,19 @@
+use std::sync::Mutex;
+
 use crate::utils::get_shape_by_id;
 use crate::GameField;
 
-pub trait Block {
+pub trait Block<'a> {
     const ID: u8;
-    fn new(x: i32, y: i32) -> Self;
+    const FIELD: Mutex<Option<&'a GameField>>;
+    fn new(field: &'a GameField, x: i32, y: i32) -> Self;
     fn rotate_clockwise(&mut self);
     fn rotate_counter_clockwise(&mut self);
     fn rotate_180(&mut self);
     fn move_left(&mut self);
     fn move_right(&mut self);
     fn move_down(&mut self);
-    fn test_collision(&self, dx: i32, dy: i32, field: &GameField) -> bool;
+    fn test_collision(&self, dx: i32, dy: i32) -> bool;
     fn get_position(&self) -> (i32, i32);
     fn get_shapes(&self) -> Vec<Vec<Vec<u8>>> {
         get_shape_by_id(Self::ID)
--- src/tetris/tetromino.rs
+++ src/tetris/tetromino.rs
@@ -1,3 +1,5 @@
+use std::sync::Mutex;
+
 use crate::contracts::Block;
 use crate::utils::OverFlowOps;
 use crate::GameField;
@@ -7,32 +9,51 @@ pub struct I {
     pub position: (i32, i32),
     pub rotate_index: i8,
 }
-/// I미노에 대한 블럭 설정입니다.
-impl Block for I {
+
+impl<'a> Block<'a> for I {
     const ID: u8 = 0;
-    /// I미노를 초기화합니다.
-    fn new(x: i32, y: i32) -> Self {
+    const FIELD: Mutex<Option<&'a GameField>> = Mutex::new(None);
+    fn new(field: &'a GameField, x: i32, y: i32) -> Self {
+        let mut field_ref = *Self::FIELD.lock().unwrap();
+        field_ref.replace(field);
         I {
             position: (x, y),
             rotate_index: 0,
         }
     }
-    /// 시계방향 회전CW)을 하는 함수입니다.
+
     fn rotate_clockwise(&mut self) {
-        /// CW시에 srs)+ 월킥 테스트를 하는 변수입니다 
         let kick_tests = [(0, 0), (-2, 0), (2, 0), (-1, 1), (2, -1)];
+
+        let mut cloned_self = self.clone();
+        cloned_self.rotate_index = cloned_self.rotate_index.add(1);
+
+        for i in kick_tests.iter() {
+            if !cloned_self.test_collision(i.0, i.1) {
+                self.position.0 += i.0;
+                self.position.1 += i.1;
+                self.rotate_index.add(1);
+                return;
+            }
+        }
 
         self.rotate_index.add(1);
     }
-    /// 반시계방향 회전(CCW)을 하는 함수입니다.
     fn rotate_counter_clockwise(&mut self) {
-        // CCW시에 srs+ 월킥을 테스트를 하는 변수입니다
         let kick_tests = [(0, 0), (-2, 0), (2, 0), (-1, 1), (2, -1)];
-        self.rotate_index.sub(1);
+        let mut cloned_self = self.clone();
+        cloned_self.rotate_index = cloned_self.rotate_index.sub(1);
+
+        for i in kick_tests.iter() {
+            if !cloned_self.test_collision(i.0, i.1) {
+                self.position.0 += i.0;
+                self.position.1 += i.1;
+                self.rotate_index.sub(1);
+                return;
+            }
+        }
     }
-    /// 180도 회전을 하는 함수입니다.
     fn rotate_180(&mut self) {
-        // 180회전시에 srs+ 월킥을 테스트를 하는 변수입니다
         let kick_tests = [
             (0, 0),
             (0, -1),
@@ -46,43 +67,54 @@ impl Block for I {
             (-1, 1),
             (1, 1),
         ];
-        self.rotate_index.add(2);
+        let mut cloned_self = self.clone();
+        cloned_self.rotate_index = cloned_self.rotate_index.add(2);
+
+        for i in kick_tests.iter() {
+            if !cloned_self.test_collision(i.0, i.1) {
+                self.position.0 += i.0;
+                self.position.1 += i.1;
+                self.rotate_index.add(2);
+                return;
+            }
+        }
     }
 
-    /// 왼쪽으로 블럭을 옴김니다.
     fn move_left(&mut self) {
         self.position.0 -= 1;
     }
-    
-    /// 오른쪽으로 블럭을 옴김니다.
+
     fn move_right(&mut self) {
         self.position.0 += 1;
     }
-    
-    /// 소프트 드랍을 합니다.
+
     fn move_down(&mut self) {
         self.position.1 += 1;
     }
 
-    /// 소프트 드랍을 합니다.
-    fn test_collision(&self, dx: i32, dy: i32, field: &GameField) -> bool {
-        let shapes = self.get_shapes();
-        let (x, y) = self.position;
-        for i in 0..4 {
-            for j in 0..4 {
-                if shapes[self.rotate_index as usize][i][j] == 1 {
-                    let x = x + j as i32 + dx;
-                    let y = y + i as i32 + dy;
-                    if x < 0 || x >= field.size.0 as i32 || y >= field.size.1 as i32 {
-                        return true;
-                    }
-                    if y >= 0 && field.blocks.contains(&(x as u8, y as u8)) {
-                        return true;
+    fn test_collision(&self, dx: i32, dy: i32) -> bool {
+        let field_ref = *Self::FIELD.lock().unwrap();
+        if let Some(field) = field_ref {
+            let shapes = self.get_shapes();
+            let (x, y) = self.position;
+            for i in 0..4 {
+                for j in 0..4 {
+                    if shapes[self.rotate_index as usize][i][j] == 1 {
+                        let x = x + j as i32 + dx;
+                        let y = y + i as i32 + dy;
+                        if x < 0 || x >= field.size.0 as i32 || y >= field.size.1 as i32 {
+                            return true;
+                        }
+                        if y >= 0 && field.blocks.contains(&(x as u8, y as u8)) {
+                            return true;
+                        }
                     }
                 }
             }
+            false
+        } else {
+            false
         }
-        false
     }
 
     fn get_position(&self) -> (i32, i32) {
